b67e4370e8efc23832ebde01e4ff8a0a82ae8493
Ethan Pippin <ethanpippin2343@gmail.com>
2021-05-21 00:06:40 -0600

    Optimize parsing and switch to json

--- Workouts.py
+++ Workouts.py
@@ -1,8 +1,5 @@
 """
     Parses the exercises from BodyBuilding.com
-    Contains the exercise title, muscle group,
-    equipment, link to the exercise page, and
-    links to the 2 exercise images.
 """
 
 from selenium import webdriver
@@ -12,14 +9,11 @@ import re
 import datetime
 import pprint
 import os
+import shutil
+import json
 
-class Excercise():
-    def __init__(self, title, muscle, equipment, link):
-        self.title = title
-        self.muscle = muscle
-        self.equipment = equipment
-        self.link = link
-
+# global variables
+equipmentPrefix = 'Equipment Type: '
 baseUrl = "https://www.bodybuilding.com/exercises/muscle/"
 muscleUrls = ['chest', 'forearms', 'lats',
             'middle-back', 'lower-back', 'neck',
@@ -29,74 +23,110 @@ muscleUrls = ['chest', 'forearms', 'lats',
             'adductors', 'abductors']
 # muscleUrls = ['chest'] # For testing
 
+# Represents an exercise
+class Excercise():
+    def __init__(self, title, equipment, link):
+        self.title = title
+        self.equipment = equipment
+        self.link = link
+        self.pictures = []
+
+# Class that controls the browser for parsing exercises
 class Br():
     def __init__(self):
         self.browser = webdriver.Chrome('./chromedriver')
-        self.url = "https://www.bodybuilding.com/"
-        self.page = 0
 
     def start(self):
-        self.browser.get(self.url)
+        self.browser.get("https://www.bodybuilding.com/")
         time.sleep(1)
 
-    def next_page(self):
-        try:
-            element = self.browser.find_element_by_class_name("ExLoadMore-btn")
-            element.click()
-            time.sleep(1)
-        except:
-            return
+    def load_whole_page(self):
+        print("\tLoading all exercises...")
+
+        # close cookie banner first so it doesn't block the load more button
+        closeCookieButton = self.browser.find_element_by_class_name('banner-close-button')
+        closeCookieButton.click()
+
+        while (True):
+            try:
+                element = self.browser.find_element_by_class_name("ExLoadMore-btn")
+                element.click()
+                time.sleep(1)
+            except:
+                return
 
     def close_out(self):
         self.browser.close()
 
     def process_page(self):
-        result = []
-        elements = self.browser.find_elements_by_class_name('ExResult-cell--nameEtc')
-        for x in elements:
+        exercises = []
+        exerciseElements = self.browser.find_elements_by_class_name('ExResult-cell--nameEtc')
+
+        print(f'\tAmount of exercises to parse: {len(exerciseElements)}...')
+
+        for x in exerciseElements:
             split = x.text.splitlines()
             link = x.find_element_by_css_selector('a').get_attribute('href')
-            exercise = Excercise(split[0], split[1], split[2], link)
-            result.append(exercise)
-            print(exercise.title)
-        self.page += 1
-        return result
 
-    def process_pictures(self, masterUrl, file):
-        self.browser.get(masterUrl)
+            # Some exercises aren't labeled with equipment
+            try:
+                parsedEquipment = split[2][split[2].index(equipmentPrefix) + len(equipmentPrefix):]
+            except:
+                parsedEquipment = ''
+
+            exercise = Excercise(split[0], parsedEquipment, link)
+
+            exercises.append(exercise)
+
+        # go through each exercise and get more detailed information
+        for exercise in exercises:
+            self.process_pictures(exercise)
+
+        return exercises
+
+    def process_pictures(self, exercise):
+        self.browser.get(exercise.link)
         time.sleep(1)
         elements = self.browser.find_elements_by_class_name('ExDetail-imgWrap')
         for x in elements:
-            img_link = x.find_element_by_css_selector('img').get_attribute('src')
-            file.write(img_link + "\n")
+            pic_link = x.find_element_by_css_selector('img').get_attribute('src')
+            exercise.pictures.append(pic_link)
 
-b = Br()
-b.start()
-os.makedirs("./exercises")
+# Main function for parsing
+def run():
+    b = Br()
+    b.start()
 
-for group in muscleUrls:
-    exercises = []
-    print("********** Currently parsing: " + muscleUrls[b.page] + " **********")
-    b.browser.get(baseUrl + muscleUrls[b.page])
-    time.sleep(1)
-    for x in xrange(20):
-        b.next_page()
+    dir = './exercises'
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.makedirs(dir)
 
-    file_name = muscleUrls[b.page]
-    complete_name = os.path.join("./exercises", file_name + ".txt")
-    file = open(complete_name, "w")
+    for group in muscleUrls:
+        exercises = []
+        print("********** Currently parsing: " + group + " **********")
+        b.browser.get(baseUrl + group)
 
-    exercises += b.process_page()
-    for x in exercises:
-        file.write(x.title + "\n")
-        file.write(x.muscle + "\n")
-        file.write(x.equipment + "\n")
-        file.write(x.link + "\n")
-        b.process_pictures(x.link, file)
-        file.write("\n")
+        # sleep a sec for waiting on load
+        time.sleep(1)
 
-    file.close()
+        # lazy load all exercises
+        b.load_whole_page()
 
-b.close_out()
+        exercises += b.process_page()
 
-print("\nDone. Get pumping.")
+        file_name = group
+        complete_name = os.path.join("./exercises", file_name + ".json")
+        file = open(complete_name, "w")
+
+        final = {group: [ob.__dict__ for ob in exercises]}
+        file.write(json.dumps(final, indent=4))
+
+        file.close()
+
+    b.close_out()
+
+    print("\nDone.")
+
+# Actually run
+run()
